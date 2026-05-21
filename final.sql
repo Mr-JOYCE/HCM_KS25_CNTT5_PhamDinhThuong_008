8b3de5c64b0e03ce018eccf1318a07f302f37913
PHẠM ĐÌNH  THƯƠNG <dinhthuong979@gmail.com>
2026-05-21 10:37:08 +0700

    upload_final_hackathon

--- final.sql
+++ final.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS Manager_Football;
-drop database manager_football;
+
 USE Manager_Football;
 
 CREATE TABLE Teams (
@@ -81,9 +81,84 @@ VALUE
     
 INSERT INTO Players(player_id, full_name, jersey_number, `position`, salary, team_id)
 VALUE 
-	( 1, '', 9, '',1),
-    ( 2, '', 17, '', , 1),
-    ( 3, '', 19, '', , 3),
-    ( 4, '', 7, '', , 2),
-    ( 5, '', 10, '', , 3);
+	( 1, 'Erling Haaland', 9, 'Forward', 450000000,1),
+    ( 2, 'Kevin De Brruyne', 17, 'Midfielder', 400000000, 1),
+    ( 3, 'Nguyen Quang Hai', 19, 'Midfielder', 60000000, 3),
+    ( 4, 'Kylian Mbappe', 7, 'Forward', 500000000, 2),
+    ( 5, 'Nguyen Van Quyet', 10, 'Forward', 55000000, 3);
+    
+INSERT INTO Matches(match_id, home_team_id, away_team_id, match_date, stadium, match_status)
+VALUE 
+	( 1, 1, 2, '2026-05-10 19:00', 'Etihad Stadium', 'Finished'),
+    ( 2, 3, 4, '2026-05-12 18:30', 'Hang Day Stadium', 'Finished'),
+    ( 3, 5, 1, '2026-05-15 20:00', 'Thien Truong Stadium', 'Schedulel'),
+    ( 4, 2, 3, '2026-05-20 21:00', 'Santiago Bemabeu', 'Schedulel'),
+    ( 5, 4, 5, '2026-05-25 17:00', 'Thong Nhat Stadium', 'Schedulel');
+    
+INSERT INTO Player_statistics(stat_id, player_id, match_id, goals, assists, yellow_cards, rating_score)
+VALUE 
+	( 1, 1, 1, 10, 1, 0, 9.5),
+    ( 2, 4, 1, 5, 0, 1, 8.2),
+    ( 3, 3, 2, 6, 2, 0, 8.5),
+    ( 4, 5, 2, 7, 0, 0, 9.0),
+    ( 5, 1, 4, 6, 0, 3, 5.0);
+    
+-- Cau 2:
+UPDATE Players
+SET salary = salary * 1.15
+WHERE `positon` = 'Forward';
+
+
+
+-- PHAN 3:
+
+-- CAU 1: 
+SELECT full_name, jersey_number, `position`
+FROM Players
+WHERE salary > 50000000 OR `position` = 'Midfielder';
+
+-- CAU 2:
+SELECT team_name, stadium
+FROM Teams
+WHERE (ranking_position BETWEEN 1 AND 5) AND team_name LIKE 'S%';
+
+-- Cau 3:
+SELECT match_id, stadium, match_date
+FROM Matches
+ORDER BY match_date 
+LIMIT 3 OFFSET 3;
+
+-- PHAN 4:
+
+-- CAU 1:
+SELECT full_name , team_name , goals , assists 
+FROM Player_statistics s
+INNER JOIN Players p 
+ON s.player_id = p.player_id
+INNER JOIN Teams t
+ON p.team_id = t.team_id;
+
+-- CAU 2:
+SELECT team_name, SUM(goals) AS TOTAL_GOAL
+FROM Player_statistics s
+INNER JOIN Players p 
+ON s.player_id = p.player_id
+INNER JOIN Teams t
+ON p.team_id = t.team_id
+GROUP BY team_name
+HAVING SUM(goals) > 10;
+
+-- CAU 3:
+SELECT player_id, full_name, salary
+FROM Players 
+ORDER BY salary DESC;
+
+-- PHAN 5:
+-- CAU 2:
+CREATE VIEW total AS
+SELECT team_name, COUNT(player_id) AS sum_player, SUM(salary) AS sum_salary
+FROM Players p
+INNER JOIN Teams t 
+ON p.team_id = t.team_id
+GROUP BY team_name;
 
